0e3b8d2ac139bfd40bf94d17e5beba3a9e00e814
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-01 11:14:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,3 +34,8 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function removeLastKitten (){
   return kittens.slice (0, kittens.length-1)
 }
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function removeFirstKitten (){
+  return kittens.slice (1)
+}
