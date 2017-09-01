727afed1879801491ac788d4c371d375df81922e
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-01 10:58:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten (name){
+  kittens.push (name);
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destuctivelyPrependKitten (name){
+  kittens.unshift (name);
+  return kittens
+}
