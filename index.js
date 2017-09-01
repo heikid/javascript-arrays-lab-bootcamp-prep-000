0402449a627a58df3ceab13a5805461d92fa792d
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-01 11:13:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,8 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function prependKitten (name){
   return [name, ...kittens]
 }
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function removeLastKitten (){
+  return kittens.slice (0, kittens.length-1)
+}
